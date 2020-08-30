e8bafb2013d20f843612a7640bbe861234cc114d
Bruno Alves <brunoalvesdasilvabr@gmail.com>
2020-08-29 23:11:32 -0300

    routing set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { MessengerComponent } from './components/messenger/messenger.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full',redirectTo:'home'},
+  {path:'home', component:HomeComponent, children:[
+    {path:'', pathMatch:'full',redirectTo:'feed'},
+    {path:'feed', pathMatch:'full', component:PostsComponent},
+    {path:'messenger',component:MessengerComponent}
+  ]},
+  {path:'profile',component:ProfileComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarLeftComponent } from './components/sidebar-left/sidebar-left.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { MessengerComponent } from './components/messenger/messenger.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarLeftComponent,
+    PostsComponent,
+    MessengerComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
